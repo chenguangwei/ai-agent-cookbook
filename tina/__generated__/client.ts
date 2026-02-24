1a397e2b480ca4b971e6f264aed49ea6225469f9
chenguangwei <chenguangwei1017@163.com>
2026-02-24 13:14:33 +0800

    Refactor code structure for improved readability and maintainability

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '17fbce772911ae4430a3f7e24ab7c48a79fc38af', queries,  });
-export default client;
-  
